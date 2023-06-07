1f05cd7bcb284bdd8517fa1c8de100f90cbd3d63
João Mota <joaommota.work@gmail.com>
2023-06-07 19:08:17 +0100

    feat(app): mockup design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,75 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { FaInstagram } from 'react-icons/fa';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className='flex flex-col items-center justify-center min-h-screen gap-10 px-10 py-10 lg:flex-row lg:px-52 lg:gap-0'>
+			<div className='flex flex-col flex-1 gap-5'>
+				<Image
+					className='relative self-center lg:self-start'
+					src='/logo-clandestino-secretly-distilled.svg'
+					alt='Gin Clandestino - Secretly Distilled Logo'
+					width={180}
+					height={40}
+					priority
+				/>
+				<div className='flex flex-col text-lg lg:text-2xl'>
+					<p>
+						Clandestino Secretly Distilled® gin was kept secret for
+						years.
+					</p>
+					<p>
+						Because we believe that some secrets aren't meant to
+						stay secret forever, we are now sharing our best-kept
+						one.
+					</p>
+					<p>
+						A meticulous blend of 3 distillations from different
+						botanical groups & Green Tree Python shed skin from the
+						Biak and Kofiau islands.
+					</p>
+					<p>
+						Each bottle is part of a special batch that marks the
+						revealing of our Clandestino gin.
+					</p>
+				</div>
+				<p className='text-lg lg:text-2xl'>
+					Thank you for being part of our history.
+				</p>
+				<hr className='h-[3px] w-2/3 lg:w-2/5 bg-[#3358A0] border-0'></hr>
+				<div className='flex items-center gap-4'>
+					<a
+						href='https://www.instagram.com/ginclandestino/'
+						target='_blank'
+						className='cursor-pointer'>
+						<FaInstagram className='text-2xl' />
+					</a>
+					<div className='text-base'>
+						<p>
+							Follow on Instagram{' '}
+							<a
+								href='https://www.instagram.com/ginclandestino/'
+								target='_blank'
+								className='cursor-pointer'>
+								@ginclandestino
+							</a>
+						</p>
+						<a href='mailto: abc@example.com' target='_blank'>
+							drink@clandestinogin.com
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className='flex-1'>
+				<Image
+					className='relative'
+					src='/clandestino-gin.jpg'
+					alt='Gin Clandestino - Secretly Distilled Bottle'
+					width={800}
+					height={1200}
+					priority
+				/>
+			</div>
+		</main>
+	);
 }
